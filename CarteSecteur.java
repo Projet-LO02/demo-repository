b91a43a7cb23daad280016cc9e817910b780e603
LucasB12348 <lucas.bermudez@laposte.net>
2024-12-05 12:58:34 +0100

    Add files via upload

--- CarteSecteur.java
+++ CarteSecteur.java
@@ -1,7 +1,14 @@
 package Cartes;
 
-public class CarteSecteur {
-	
+public class CarteSecteur extends Carte {
+	private Secteur secteur;
+	public CarteSecteur(Secteur secteur) {
+		super(typeDeCarte.SECTEUR);
+		this.secteur=secteur;
+	}
+	public String toString() {
+		return "Type de carte : "+this.getType()+"\nSecteur :\n\t id du secteur : "+this.secteur.getId();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
